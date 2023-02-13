522e1901ea51766daebe257be14d9871a87f1730
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2023-02-13 16:35:55 +0100

    Default handler error message implementation

--- handler/defaultHandler.go
+++ handler/defaultHandler.go
@@ -3,5 +3,5 @@ package handler
 import "net/http"
 
 func DefaultPathHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "The default site.", http.StatusOK)
+	http.Error(w, "The site you are trying to reach does not exist.", http.StatusNotFound)
 }
