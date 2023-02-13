c456dc0e05e9509db5854eaf89cc8c0e289b9ca4
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2023-02-13 17:45:26 +0100

    Implemented method checker to allow only GET

--- handler/universityHandler.go
+++ handler/universityHandler.go
@@ -3,5 +3,11 @@ package handler
 import "net/http"
 
 func UniversityHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "The university information site.", http.StatusOK)
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "Only "+http.MethodGet+" requests are allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	http.Error(w, "The university information site. "+r.Method, http.StatusOK)
 }
