780627c4015e1358f2fe0e5bd000caa20f8bdccd
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2023-02-13 14:46:57 +0100

    Global path constants initial implementation

---
+++ global/constants.go
@@ -0,0 +1,7 @@
+package global
+
+// Resource root paths
+const DEFAULT_PATH = "/"
+const UNI_INFO_PATH = "/unisearcher/v1/uniinfo/"
+const NEIGHBOUR_UNI_PATH = "/unisearcher/v1/neighbourunis/"
+const DIAG_PATH = "/unisearcher/v1/diag/"
