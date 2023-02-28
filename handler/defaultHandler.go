43a90835f53f414f2fe70b83d71d1c96c5aa7725
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2023-02-28 17:22:27 +0100

    Documentation in default handler

--- handler/defaultHandler.go
+++ handler/defaultHandler.go
@@ -2,6 +2,10 @@ package handler
 
 import "net/http"
 
+/*
+	Default path handler will just display an error message for the
+	user, telling the user that the site does not exist
+*/
 func DefaultPathHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "The site you are trying to reach does not exist.", http.StatusNotFound)
 }
