6115f8b7a3ba1176c6a17562dc20e76cd38e4245
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2023-03-02 00:31:08 +0100

    Added documentatio on diag handler

--- handler/diagnosticsHandler.go
+++ handler/diagnosticsHandler.go
@@ -8,25 +8,34 @@ import (
 	"time"
 )
 
-/*Some action*/
+/*
+The diagnostics handler provides all operations of the diag/
+endpoint. This includes getting the status of the third-party
+apis to see if they are available, as well as version and
+uptime of the server
+*/
 func DiagnosticsHandler(w http.ResponseWriter, r *http.Request) {
+	// Make sure only GET methods are allowed
 	if r.Method != http.MethodGet {
 		http.Error(w, "This site only allows "+http.MethodGet+" methods.", http.StatusMethodNotAllowed)
 		return
 	}
+	// Add content-type json to the response header
 	w.Header().Add("content-type", "application/json")
 
+	// Send a GET request to the University API
 	universityAPI, err := request.SendGETRequest(global.UNIVERSITY_API_URL)
 	if err != nil {
 		http.Error(w, "Something went in the universityAPI request", http.StatusInternalServerError)
 		return
 	}
+	// Send a GET request to the Country API
 	countryAPI, err1 := request.SendGETRequest(global.UNIVERSITY_API_URL)
 	if err1 != nil {
-		http.Error(w, "Something went in the universityAPI request", http.StatusInternalServerError)
+		http.Error(w, "Something went in the countryAPI request", http.StatusInternalServerError)
 		return
 	}
-
+	// Make a diagnostics struct based on api statuses, version and uptime of server
 	diagnostics := global.DiagnostictStruct{
 		UniversityAPI: universityAPI.Status,
 		CountryAPI:    countryAPI.Status,
@@ -34,10 +43,10 @@ func DiagnosticsHandler(w http.ResponseWriter, r *http.Request) {
 		Uptime:        int(time.Now().Unix() - global.START_TIME),
 	}
 
+	// Encode the struct to JSON and send to client
 	encoder := json.NewEncoder(w)
-
 	err2 := encoder.Encode(diagnostics)
-
+	// Handle any errors that may occur while encodig the JSON
 	if err2 != nil {
 		http.Error(w, "Error while encoding JSON", http.StatusInternalServerError)
 		return
