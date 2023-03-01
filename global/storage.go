ce7f8c804d5104869babcbf37e64b3cb1eb53844
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2023-03-01 23:19:55 +0100

    Added documentation on storage

--- global/storage.go
+++ global/storage.go
@@ -7,6 +7,10 @@ import (
 // Storage collection to store all country structs to reduce api calls
 var countrymap = map[string]Country{}
 
+/*
+* Add a country struct to the sotrage collection to reduce
+* api calls. If country already is in the collection, false is returned
+ */
 func AddCountryToStorage(alpha_2 string, country Country) bool {
 	// Check if country is already in the map
 	if _, ok := countrymap[alpha_2]; ok {
@@ -19,6 +23,11 @@ func AddCountryToStorage(alpha_2 string, country Country) bool {
 	}
 }
 
+/*
+* Return a country struct from the storage collection based on
+* search word and search type. False is returned if country is
+* not in collecion
+ */
 func GetCountryFromStorage(search string, searchType string) (Country, bool) {
 	switch searchType {
 	case "cca2":
@@ -27,18 +36,22 @@ func GetCountryFromStorage(search string, searchType string) (Country, bool) {
 		// Return the country and the status code if the conutry is in the map
 		return val, ok
 	case "cca3":
+		// Loop through all countries in the map
 		for _, country := range countrymap {
+			//Return conutry if CCA3 is a match
 			if country.CCA3 == search {
 				return country, true
 			}
 		}
 	case "name":
+		// Loop through all countries in the map
 		for _, country := range countrymap {
-			name := country.Name["common"].(string)
+			name := country.Name["common"].(string) // Retrieve the common name of the country
+			//Return conutry if name is a match
 			if strings.ToLower(name) == strings.ToLower(search) {
 				return country, true
 			}
 		}
 	}
-	return Country{}, false
+	return Country{}, false // Return false if country is not in the storage
 }
