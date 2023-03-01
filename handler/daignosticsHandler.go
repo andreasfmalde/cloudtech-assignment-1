657842427d659013c39d362ef745ad0781b7e90c
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2023-03-01 18:33:31 +0100

    Implemented the diagnostics handler

--- handler/daignosticsHandler.go
+++ handler/daignosticsHandler.go
@@ -1,10 +1,45 @@
 package handler
 
 import (
+	"assignment-1/global"
+	"encoding/json"
 	"net/http"
+	"time"
 )
 
 /*Some action*/
 func DiagnosticsHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "The diagnostics site.", http.StatusOK)
+	if r.Method != http.MethodGet {
+		http.Error(w, "This site only allows "+http.MethodGet+" methods.", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Header().Add("content-type", "application/json")
+
+	universityAPI, err := sendGetRequest(global.UNIVERSITY_API_URL)
+	if err != nil {
+		http.Error(w, "Something went in the universityAPI request", http.StatusInternalServerError)
+		return
+	}
+	countryAPI, err1 := sendGetRequest(global.UNIVERSITY_API_URL)
+	if err1 != nil {
+		http.Error(w, "Something went in the universityAPI request", http.StatusInternalServerError)
+		return
+	}
+
+	diagnostics := global.DiagnostictStruct{
+		UniversityAPI: universityAPI.Status,
+		CountryAPI:    countryAPI.Status,
+		Version:       "v1",
+		Uptime:        int(time.Now().Unix() - global.START_TIME),
+	}
+
+	encoder := json.NewEncoder(w)
+
+	err2 := encoder.Encode(diagnostics)
+
+	if err2 != nil {
+		http.Error(w, "Error while encoding JSON", http.StatusInternalServerError)
+		return
+	}
+
 }
